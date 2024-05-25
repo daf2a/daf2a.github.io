5df5c575a71add369e9443ad594eb2f0e125d528
daf2a <muhammaddaffaashdaqfillah@gmail.com>
2024-05-26 00:38:53 +0700

    refactor: Update TracingBeam component to adjust y1 and y2 values for better animation

--- src/components/ui/tracing-beam.tsx
+++ src/components/ui/tracing-beam.tsx
@@ -43,14 +43,14 @@ export const TracingBeam = ({
   }, []);
 
   const y1 = useSpring(
-    useTransform(scrollYProgress, [0, 0.4], [50, svgHeight]),
+    useTransform(scrollYProgress, [0, 0.4], [0, svgHeight - 300]),
     {
       stiffness: 500,
       damping: 90,
     }
   );
   const y2 = useSpring(
-    useTransform(scrollYProgress, [0, 1], [50, svgHeight - 300]),
+    useTransform(scrollYProgress, [0, 1], [0, svgHeight - 300]),
     {
       stiffness: 500,
       damping: 90,
