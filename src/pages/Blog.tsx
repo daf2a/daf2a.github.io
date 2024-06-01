449fd7287c6274a4116b8f5d10c411928edc88b9
daf2a <muhammaddaffaashdaqfillah@gmail.com>
2024-06-01 19:39:38 +0700

    feat: blog

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,100 +1,56 @@
 import { BsCalendarDate } from "react-icons/bs";
+import { MultiStepLoader as Loader } from "@/components/ui/multi-step-loader";
 import { useEffect, useState } from "react";
-import { LuConstruction } from "react-icons/lu";
+import { BlockMapType } from "react-notion";
+import axios from "axios";
+import "react-notion/src/styles.css";
+import "prismjs/themes/prism-tomorrow.css";
+import { NotionRenderer } from "react-notion";
+import { motion } from "framer-motion";
+import { Button } from "@/components/ui/button";
 
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-} from "@/components/ui/dialog";
+const loadingStates = [
+  {
+    text: "Fetching data from Notion",
+  },
+  {
+    text: "Building the Blog",
+  },
+  {
+    text: "Adding finishing touches",
+  },
+];
 
-// Blog post interface
 interface BlogPost {
   title: string;
   description: string;
   date: string;
-  link: string;
   image: string;
+  onClick: () => void;
 }
 
-// Blog card data as an array of objects
-const blogPosts: BlogPost[] = [
-  {
-    title: "Unlocking the Secrets of Sustainable Living",
-    description:
-      "Discover practical tips and strategies to reduce your carbon footprint and live a more eco-friendly lifestyle.",
-    date: "May 15, 2023",
-    link: "#",
-    image: "/placeholder.svg",
-  },
-  {
-    title: "Unlocking the Secrets of Sustainable Living",
-    description:
-      "Discover practical tips and strategies to reduce your carbon footprint and live a more eco-friendly lifestyle.",
-    date: "May 15, 2023",
-    link: "#",
-    image: "/placeholder.svg",
-  },
-  {
-    title: "Unlocking the Secrets of Sustainable Living",
-    description:
-      "Discover practical tips and strategies to reduce your carbon footprint and live a more eco-friendly lifestyle.",
-    date: "May 15, 2023",
-    link: "#",
-    image: "/placeholder.svg",
-  },
-  {
-    title: "Unlocking the Secrets of Sustainable Living",
-    description:
-      "Discover practical tips and strategies to reduce your carbon footprint and live a more eco-friendly lifestyle.",
-    date: "May 15, 2023",
-    link: "#",
-    image: "/placeholder.svg",
-  },
-  {
-    title: "Unlocking the Secrets of Sustainable Living",
-    description:
-      "Discover practical tips and strategies to reduce your carbon footprint and live a more eco-friendly lifestyle.",
-    date: "May 15, 2023",
-    link: "#",
-    image: "/placeholder.svg",
-  },
-  {
-    title: "Unlocking the Secrets of Sustainable Living",
-    description:
-      "Discover practical tips and strategies to reduce your carbon footprint and live a more eco-friendly lifestyle.",
-    date: "May 15, 2023",
-    link: "#",
-    image: "/placeholder.svg",
-  },
-];
+interface Blog {
+  id: string;
+  title: string;
+  deskripsi: string;
+  img_url: string;
+  created_time: string;
+  properties: BlockMapType;
+}
 
-function BlogCard({ title, description, date}: BlogPost) {
-  const [imageUrl, setImageUrl] = useState("");
-
-  useEffect(() => {
-    const fetchRandomImage = async () => {
-      const response = await fetch(
-        "https://source.unsplash.com/random/200x100/"
-      );
-      if (response.ok) {
-        setImageUrl(response.url);
-      } 
-    };
-
-    fetchRandomImage();
-  }, []);
-
+function BlogCard({ title, description, date, image, onClick }: BlogPost) {
   return (
-    <div className="flex flex-col sm:flex-row items-center text-left gap-4 rounded-lg shadow-sm dark:border px-8 py-8 mt-4 lg:mt-0">
+    <div
+      className="flex flex-col sm:flex-row items-center text-left gap-4 rounded-lg shadow-sm dark:border px-8 py-8 mt-4 lg:mt-0 cursor-pointer"
+      onClick={onClick}
+    >
       <img
-        src={imageUrl} 
-        alt="Random Image from Unsplash"
+        src={image || "/placeholder.svg"}
+        alt="Blog Image"
         width={200}
         height={100}
         className="w-full sm:w-[30%] rounded-lg object-cover bg-zinc-900"
-        onError={() => setImageUrl("/placeholder.svg")} // Error handling
+        onError={(e) => (e.currentTarget.src = "/placeholder.svg")} // Error handling
       />
       <div className="space-y-1 w-full sm:w-[70%] md:p-4">
         <h2 className="text-lg font-semibold">{title}</h2>
@@ -110,34 +66,99 @@ function BlogCard({ title, description, date}: BlogPost) {
   );
 }
 
-export default function Blog() {
-  const [isDialogVisible, setIsDialogVisible] = useState(false);
+function Blog() {
+  const [data, setData] = useState<Blog[]>([]);
+  const [selectedBlog, setSelectedBlog] = useState<Blog | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setIsDialogVisible(true);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://be-daf2a.vercel.app/api/notion-blog"
+        );
+
+        if (response.status !== 200) {
+          throw new Error("Notion API request failed");
+        }
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching Notion data:", error);
+      } finally {
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
+  const formatDate = (dateString: string) => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
+  const handleCardClick = (blog: Blog) => {
+    setSelectedBlog(blog);
+  };
+
+  const handleBackClick = () => {
+    setSelectedBlog(null);
+  };
+
   return (
-    <div className="mx-auto py-4 md:py-12 px-8 md:px-12 lg:px-24 lg:grid lg:grid-cols-2 lg:gap-4 blurred-content">
-      {isDialogVisible && (
-        <Dialog open={isDialogVisible} onOpenChange={setIsDialogVisible}>
-          <DialogContent>
-            <DialogHeader>
-              <DialogDescription>
-                  <div className="py-3 flex gap-4 items-center justify-center text-zinc-400">
-                    <LuConstruction className="text-zinc-400 flex-shrink-0 w-16 h-16 text-primary-foreground" />
-                    <p className="text-left font-semibold">
-                    This page is currently under construction. Please check back later.
-                    </p>
-                  </div>
-                </DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
+    <>
+      {isLoading && (
+        <Loader
+          loadingStates={loadingStates}
+          loading={isLoading}
+          duration={1000}
+        />
       )}
-      {blogPosts.map((post) => (
-        <BlogCard key={post.title} {...post} />
-      ))}
-    </div>
+      {selectedBlog ? (
+        <motion.div
+          initial={{ opacity: 0.0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.5,
+            ease: "easeInOut",
+          }}
+          className=""
+        >
+          <div className="p-8 item-left text-left flex flex-col items-end bg-zinc-200 h-screen relative">
+            <Button
+              onClick={handleBackClick}
+              variant="outline"
+              className="mb-4 -mt-4 sticky top-20 left-4"
+            >
+              Back to Blog List
+            </Button>
+            <div className="self-start">
+              <NotionRenderer blockMap={selectedBlog.properties} />
+            </div>
+          </div>
+        </motion.div>
+      ) : (
+        <div className="mx-auto py-4 md:py-12 px-8 md:px-12 lg:px-24 lg:grid lg:grid-cols-2 lg:gap-4">
+          {data.map((blog) => (
+            <BlogCard
+              key={blog.id}
+              title={blog.title}
+              description={blog.deskripsi}
+              date={formatDate(blog.created_time)}
+              image={blog.img_url}
+              onClick={() => handleCardClick(blog)}
+            />
+          ))}
+        </div>
+      )}
+    </>
   );
 }
+
+export default Blog;
