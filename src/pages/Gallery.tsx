afbaeec3e9202f59bc9e65d58a27be574c439296
daf2a <muhammaddaffaashdaqfillah@gmail.com>
2024-06-02 16:37:43 +0700

    feat: time before gallery

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -37,6 +37,7 @@ export default function Gallery() {
       } catch (error) {
         console.error("Error fetching Notion data:", error);
       } finally {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
         setIsLoading(false);
       }
     };
